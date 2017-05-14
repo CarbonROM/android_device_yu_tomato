9f2a129c4ed912a02660ad24dff4bdafb11c6660
Ritesh Saxena <riteshsax007@gmail.com>
2017-05-14 05:49:28 +0000

    Carbon Bringup
    Signed-off-by: Ritesh Saxena <riteshsax007@gmail.com>

--- carbon_tomato.mk
+++ carbon_tomato.mk
@@ -14,13 +14,14 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common CarbonROM stuff.
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := lineage_tomato
+PRODUCT_NAME := carbon_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
